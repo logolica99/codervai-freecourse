035a06cb9637544cffb1889082eb433b18d29dee
logolica99 <jamijubaer@gmail.com>
2024-02-12 22:50:55 +0600

    added anaylitcs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KWPCFCFTRP"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KWPCFCFTRP');
+  </script>`,
+          }}
+        ></script>
+      </Head>
 
       <body>
         <Main />
